dfe9cc7ee4009e02b1ee702e7a6881febd8bab6a
Ram Prabodh Induri <46286766+InduriRam@users.noreply.github.com>
2020-05-31 00:54:36 +0530

    Calculator application using Tkinter

---
+++ Calculator.py
@@ -0,0 +1,106 @@
+from tkinter import*
+root = Tk()
+root.title("Simple Calculator")
+e = Entry(root, width=35, borderwidth = 5)
+e.grid(row = 0, column = 0,columnspan = 3, padx= 10, pady = 10) #columnspan tells us no of columns we want our object to be, padx and pady are paddings
+def button_click (number):
+    #e.delete(0,END)
+    current = e.get()
+    e.delete(0,END)
+    e.insert(0,str(current) + str(number))
+def button_clr():
+    e.delete(0,END)
+
+def button_plus():
+    global f_num
+    first_num = e.get()
+    f_num = int(first_num)
+    global math
+    math="addition"
+    e.delete(0,END)
+
+def button_minus():
+    global f_num
+    first_num = e.get()
+
+    f_num = int(first_num)
+    global math
+
+    math = "subtraction"
+    e.delete(0, END)
+
+def button_slash():
+    first_num = e.get()
+    global f_num
+    f_num = int(first_num)
+    global math
+
+    math = "division"
+    e.delete(0, END)
+
+def button_star():
+    global f_num
+    first_num = e.get()
+
+    f_num = int(first_num)
+    global math
+
+
+    math = "multiplication"
+    e.delete(0, END)
+
+
+def button_equal():
+    global f_num
+    global math
+    second_num = e.get()
+    e.delete(0,END)
+    second = int(second_num)
+    if(math == "addition"):
+        e.insert(0,int(second) + int(f_num))
+    elif(math == "subtraction"):
+        e.insert(0,int(f_num)-int(second))
+    elif(math=="multiplication"):
+        e.insert(0,int(f_num) * int(second) )
+    elif (math == "division"):
+        if(int(second == 0)):
+            e.insert(0,"- -")
+        else:
+            e.insert(0, int(f_num)//int(second))
+
+button_1 = Button(root,text="1",padx=40,pady=20,command=lambda :  button_click(1))
+button_2 = Button(root,text="2",padx=40,pady=20,command=lambda :  button_click(2))
+button_3 = Button(root,text="3",padx=40,pady=20,command=lambda :  button_click(3))
+button_4 = Button(root,text="4",padx=40,pady=20,command=lambda :  button_click(4))
+button_5 = Button(root,text="5",padx=40,pady=20,command=lambda :  button_click(5))
+button_6 = Button(root,text="6",padx=40,pady=20,command=lambda :  button_click(6))
+button_7 = Button(root,text="7",padx=40,pady=20,command=lambda :  button_click(7))
+button_8 = Button(root,text="8",padx=40,pady=20,command=lambda :  button_click(8))
+button_9 = Button(root,text="9",padx=40,pady=20,command=lambda :  button_click(9))
+button_0 = Button(root,text="0",padx=40,pady=20,command=lambda :  button_click(0))
+button_clear = Button(root,text="Clear",padx=79,pady=20,command= lambda:button_clr())
+button_eql = Button(root,text="=",padx=90,pady=20,command=button_equal)
+button_add = Button(root,text="+",padx=40,pady=20,command=button_plus)
+button_sub = Button(root,text="-",padx=41,pady=20,command=button_minus)
+button_div = Button(root,text="/",padx=41,pady=20,command=button_slash)
+button_mul = Button(root,text="*",padx=41,pady=20,command=button_star)
+
+button_1.grid(row = 3, column =0)
+button_2.grid(row = 3, column =1)
+button_3.grid(row = 3, column =2)
+button_4.grid(row = 2, column =0)
+button_5.grid(row = 2, column =1)
+button_6.grid(row = 2, column =2)
+button_7.grid(row =1 , column =0)
+button_8.grid(row = 1, column =1)
+button_9.grid(row = 1, column =2)
+button_0.grid(row = 4, column = 0)
+button_clear.grid(row = 4, column = 1, columnspan = 2)
+button_eql.grid(row = 5,column = 0,columnspan = 2)
+button_add.grid(row = 5,column = 2)
+button_sub.grid(row = 6, column = 0)
+button_mul.grid(row = 6, column = 1)
+button_div.grid(row = 6, column = 2)
+
+
+root.mainloop()
